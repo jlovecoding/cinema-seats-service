a3f8187b4f326cd7f7565c82dcad6a5a36990ba9
jlovecoding <jlovecoding@gmail.com>
2019-04-04 18:57:03 +0200

    add log for incoming requests

--- src/main/java/com/cinema/seats/controller/SeatController.java
+++ src/main/java/com/cinema/seats/controller/SeatController.java
@@ -2,6 +2,7 @@ package com.cinema.seats.controller;
 
 import com.cinema.seats.model.Seat;
 import com.cinema.seats.repository.SeatRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static java.lang.String.format;
 import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE_TIME;
 
 @RestController
 @RequestMapping("/seats")
+@Slf4j
 public class SeatController {
 
     @Autowired
@@ -26,6 +29,8 @@ public class SeatController {
             @RequestParam final Integer screen,
             @RequestParam @DateTimeFormat(iso = DATE_TIME) final LocalDateTime start,
             @RequestParam final Boolean available) {
+        log.info(format("findByScreenAndStartAndAvailable(screen=%s, start=%s, available=%s",
+                screen, start, available));
         return seatRepository.findByScreenAndStartAndAvailable(screen, start, available);
     }
 }
